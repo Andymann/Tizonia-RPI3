e5a42d31eedf907c5aee9fcf295c49afec1e4aa8
Andy Fischer <info@andyland.info>
2021-04-27 18:34:03 +0200

    fix bug in sync from github

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo running autostart > autostart.log
+#echo running autostart > autostart.log
 pkill tizonia
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 #OID=$(wget -q -nv -O - https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/updateID.txt >/dev/null 2>/dev/null | grep -v '#')
 OID=$(wget -q -nv -O - https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/updateID.txt 2>/dev/null | grep -v '#')
-LID=$(grep -v '#' updateID.txt)
+LID=$(grep -v '#' $DIR/updateID.txt)
 if [ ! -z "$OID" ]; then
 	if (test $OID -eq $LID); then
 		echo "gleich"	
@@ -12,13 +13,13 @@ if [ ! -z "$OID" ]; then
 	else
 		#echo "ungleich"
 		#values differ: update required
-		rm ./tizonia.py.old >/dev/null 2>/dev/null
-		mv ./tizonia.py tizonia.py.old >/dev/null 2>/dev/null
-		wget https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/tizonia.py
+		rm $DIR/tizonia.py.old >/dev/null 2>/dev/null
+		mv $DIR/tizonia.py $DIR/tizonia.py.old >/dev/null 2>/dev/null
+		wget --directory-prefix=$DIR https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/tizonia.py
 
 		#Remove previous updateID.txt and fetch the updated one
-		rm updateID.txt >/dev/null 2>/dev/null
-		wget https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/updateID.txt
+		rm $DIR/updateID.txt >/dev/null 2>/dev/null
+		wget --directory-prefix=$DIR https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/updateID.txt
 	fi
 else
 	echo "Variable nicht gesetzt oder offline"
@@ -27,6 +28,6 @@ fi
 
 
 while true; do
-	python3 ./tizonia.py
+	python3 $DIR/tizonia.py
 	sleep 5
 done
--- tizonia.py
+++ tizonia.py
@@ -10,9 +10,199 @@ import threading
 import psutil
 import re
 import dbus
+import sys
 
+TIMERVALUE = .01
+STATE_NONE = -2
+STATE_UNDEFINED = -1
+STATE_PLAYING = 1
+STATE_BOOTING = 2
+STATE_LOADINGPLAYLIST = 3
+STATE_STARTINGTIZONIA = 4
+STATE_STOPPED = 5
+
+iLastButton = -1
+iCounter = 0
+iBlinkCounter = 0
+bLed = True
+iState = STATE_UNDEFINED
+iErrorCounter = 0
 
 lst_playlist = []
+iActivePlaylist = -1
+iTimerCounter = -1
+
+
+def initGpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    # ----GREEN Led
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.output(13, 1)  # OFF
+
+    GPIO.add_event_detect(11, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+    GPIO.add_event_detect(8, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+    GPIO.add_event_detect(10, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+    GPIO.add_event_detect(12, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+    GPIO.add_event_detect(16, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+    GPIO.add_event_detect(18, GPIO.RISING, callback=doIfHigh, bouncetime=500)
+
+
+def doIfHigh(channel):
+    global iLastButton
+    iLastButton = channel
+
+
+def buttonPressed(pButton):
+    global iActivePlaylist
+    # print('buttonPressed:' + str(pButton))
+    if pButton == 11:
+        if not getPlayer() is None:
+            getPlayer().Next(dbus_interface='org.mpris.MediaPlayer2.Player')
+        else:
+            print('buttonPressed: Skip aber Tizonia ist nicht gestartet')
+            # Problem
+            sys.exit()
+    if pButton == 8:
+        iActivePlaylist = 0
+        killTizonia()
+        startTizonia(iActivePlaylist)
+    if pButton == 10:
+        iActivePlaylist = 1
+        killTizonia()
+        startTizonia(iActivePlaylist)
+    if pButton == 12:
+        iActivePlaylist = 2
+        killTizonia()
+        startTizonia(iActivePlaylist)
+    if pButton == 16:
+        iActivePlaylist = 3
+        killTizonia()
+        startTizonia(iActivePlaylist)
+    if pButton == 18:
+        iActivePlaylist = 4
+        killTizonia()
+        startTizonia(iActivePlaylist)
+
+
+def processState():
+    global iState
+    global iErrorCounter
+    global bLed
+    if iState == STATE_UNDEFINED:
+        global iBlinkCounter
+        iBlinkCounter = iBlinkCounter + 1
+        if iBlinkCounter == 100:
+            iBlinkCounter = 0
+            bLed = not bLed
+            GPIO.output(13, bLed)
+    if iState == STATE_LOADINGPLAYLIST:
+        iBlinkCounter = iBlinkCounter + 1
+        if iBlinkCounter == 25:
+            iBlinkCounter = 0
+            bLed = not bLed
+            GPIO.output(13, bLed)
+    if iState == STATE_STOPPED:
+        bLed = False
+        print('processState:Stopped')
+        GPIO.output(13, not bLed)  # Inverse Logik
+        iState = STATE_NONE
+    if iState == STATE_STARTINGTIZONIA:
+        # Tizonia wurde aufgerufen, die Fahrstuhlmusik spielt.
+        # Es kann jetzt beliebig lange dauern, bis die
+        # Playlist geladen wurde und die Audiowiedergabe beginnt.
+        #
+        # Bis dahin ergibt eine Query auf den Player das Ergebnis 'Stopped', danach 'Playing'
+        global iTimerCounter
+        if iTimerCounter < 100:
+            iTimerCounter = iTimerCounter + 1
+        else:
+            # print('processState: starting. Zeit fuer Query')
+            bLed = not bLed
+            GPIO.output(13, bLed)
+            iTimerCounter = 0
+            if not getPlayer() is None:
+                stat = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus',
+                                       dbus_interface='org.freedesktop.DBus.Properties')
+                # print('status:' + str(stat))  # z.B. 'Playing
+                if 'Stopped' in str(stat):
+                    pass
+                if 'Playing' in str(stat):
+                    # print('Tizonia spielt Musik. Stoppe Fahrstuhlmusik')
+                    mixer.Channel(0).stop()
+                    bLed = True
+                    GPIO.output(13, not bLed)
+                    iState = STATE_PLAYING
+                iErrorCounter = 0
+            else:
+                print('Tizonia not running')
+                # Das kann passieren, wenn der Aufruf und die Abfrage zeitlich she rnah beieinander sind.
+                # Wenn das mehr als zweimal nacheinander geschieht, wird es kritisch.
+                iErrorCounter = iErrorCounter + 1
+    if iState == STATE_PLAYING:
+        # Alle 2 Sekunden oder so mal schauen, was gerade so los ist
+        if iTimerCounter < 200:
+            iTimerCounter = iTimerCounter + 1
+        else:
+            iTimerCounter = 0
+            if not getPlayer() is None:
+                stat = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus',
+                                       dbus_interface='org.freedesktop.DBus.Properties')
+                # print('MODE_PLAYING. Echter Status:' +
+                #      str(stat))  # z.B. 'Playing
+                iErrorCounter = 0
+            else:
+                print('MODE_PLAYING aber Tizonia laeuft nicht!')
+                # Hier haben wir theoretisch ein Problem
+                # Wir beenden, das Script drumherum kuemmert sich um einen Neuistart nach 5 Sekunden
+                iErrorCounter = iErrorCounter + 1
+
+
+def getXml():
+    global iState
+    iState == STATE_LOADINGPLAYLIST
+    try:
+        var_url = urlopen(
+            'https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/tizonia.xml')
+        tree = ET.parse(var_url)
+        root = tree.getroot()
+        print('Playlist IDs:')
+        if len(root) >= 5:
+            print('XML ist gross genug')
+            for playlist in root.findall('playlist'):
+                name = playlist.get('name')
+                plid = playlist.find('id').text
+                print(name + "  " + plid)
+                lst_playlist.append(plid)
+            iState = STATE_UNDEFINED
+            return True
+        else:
+            print('XML zu klein')
+            iState = STATE_UNDEFINED
+            return False
+    except:
+        print('Error while parsing xml from web')
+        iState = STATE_UNDEFINED
+        return True
+
+
+def killTizonia():
+    print('Terminating Tizonia ...')
+    GPIO.output(13, 1)  # OFF
+    PROCNAME = "tizonia"
+    for proc in psutil.process_iter():
+        if proc.name() == PROCNAME:
+            proc.kill()
+            print('Tizonia process killed')
+    global iState
+    iState = STATE_STOPPED
+    # print('done')
 
 
 def getPlayer():
@@ -26,243 +216,53 @@ def getPlayer():
         return player
 
 
-def killTizonia():
-    print('Terminating Tizonia ...')
-    GPIO.output(13, 1)  # OFF
-    PROCNAME = "tizonia"
-    for proc in psutil.process_iter():
-        if proc.name() == PROCNAME:
-            proc.kill()
-            print('Tizonia process killed')
-
-
-def isRunning():
-    PROCNAME = "tizonia"
-    for proc in psutil.process_iter():
-        if proc.name() == PROCNAME:
-            # print("isRunning: True")
-            GPIO.output(13, 0)  # ON
-            return True
-    print("isRunning: False")
-    return False
-
-
 def startTizonia(playlistID):
-    # Reset Timer
-    # print('startTizonia')
-    global t
-    t = time.time()+4
-    print('starting Fahrstuhlmusik')
-    # mixer.sound.play(-1)
-    # mixer.Channel(0).play(-1)
+    global iState
+    iState = STATE_STARTINGTIZONIA
+    # print('starting Fahrstuhlmusik')
     sound1 = mixer.Sound('/home/pi/Music/jeopardy.wav')
     mixer.Channel(0).play(sound1, -1)
     print('Starting with Playlist ' + lst_playlist[playlistID])
     output = subprocess.call(
         'lxterminal -e tizonia --spotify-playlist-id ' + lst_playlist[playlistID] + ' -s &', cwd='/home/pi/', shell=True)
-    print(output)
-    print('Tizonia started...')
+    # wenn output == 0 ist erstmal alles in Ordnung
+    # es kann aber jetzt noch dauern, bis die Playlist eingelesen wurde und die
+    # Audiowiedergabe beginnt.
+    print('Tizonia started. Status:' + str(output))
 
 
-def doIfHigh(channel):
-    # Zugriff auf Variable i ermöglichen
-    # global i
-    # Wenn Eingang HIGH ist, Ausgabe im Terminal erzeugen
-    time.sleep(.15)
-    # testval = GPIO.input(channel)
-    # print('testval:' + str(testval))
-    # start_time = time.time()
-
-    # while GPIO.input(channel) == 1:
-    #     pass
-
-    # buttonTime = time.time() - start_time
-    # if buttonTime >= .02:
-    if GPIO.input(channel) == 1:
-        print('Long button Press')
-        print("Eingang " + str(channel) + " HIGH")
-        global sCmd
-        if channel == 11:
-            sCmd = 'n'
-        if channel is 8:
-            print('Playlist Index 0')
-            sCmd = 'p0'
-        if channel is 10:
-            print('Playlist Index 1')
-            sCmd = 'p1'
-        if channel is 12:
-            print('Playlist Index 2')
-            sCmd = 'p2'
-        if channel is 16:
-            print('Playlist Index 3')
-            sCmd = 'p3'
-        if channel is 18:
-            print('Playlist Index 4')
-            sCmd = 'p4'
-
-
-try:
-    var_url = urlopen(
-        'https://raw.githubusercontent.com/Andymann/Tizonia-RPI3/master/tizonia.xml')
-    tree = ET.parse(var_url)
-
-    root = tree.getroot()
-    print('Playlist IDs:')
-
-    if len(root) >= 5:
-        print('XML ist gross genug')
-        for playlist in root.findall('playlist'):
-            name = playlist.get('name')
-            plid = playlist.find('id').text
-            print(name + "  " + plid)
-            lst_playlist.append(plid)
-        else:
-            print('XML zu klein')
-except:
-    print('Error while parsing xml from web')
-
-global t
-t = time.time()
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# ----GREEN Led
-GPIO.setup(13, GPIO.OUT)
-GPIO.output(13, 1)  # OFF
-
-GPIO.add_event_detect(11, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-GPIO.add_event_detect(8, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-GPIO.add_event_detect(10, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-GPIO.add_event_detect(12, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-GPIO.add_event_detect(16, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-GPIO.add_event_detect(18, GPIO.RISING, callback=doIfHigh, bouncetime=500)
-
-
-idActive = -1
-
-
-mixer.init()
-# channel1 = pygame.mixer.Channel(0) # argument must be int
-# sound1 = pygame.mixer.Sound('/home/pi/Music/jeopardy.mp3')
-
-# channel1.play(sound1, loops = -1)
-# pygame.mixer.find_channel().play(sound1)
-
-# mixer.music.load('/home/pi/Music/jeopardy.mp3')
-
-
-ir = randrange(5)
-print('Autostart. picking random playlist:' + str(ir))
-startTizonia(ir)
-
-sCmd = 'NONO'
-while True:
-
-    if time.time()-t > 4:
-        # run your task here every n seconds
-        t = time.time()
-        # if not getPlayer() is None:
-        #    try:
-        #        stat = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus',
-        #                               dbus_interface='org.freedesktop.DBus.Properties')
-        #        print(stat)
-        #        if 'Playing' in stat:
-        #            mixer.music.stop()
-        #
-        #    except Exception as err:
-        #        print('Exception:')
-        if isRunning():
-            try:
-                stat = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus',
-                                       dbus_interface='org.freedesktop.DBus.Properties')
-                # print(stat) 'Playing'
-                if 'Playing' in stat:
-                    # print('Stopping Fahrstuhlmusik...')
-                    mixer.Channel(0).stop()
-
-            except Exception as err:
-                print('Exception while querying Tizonia instance:')
-        else:
-            print('Interval query: Tizonia not running')
-            if idActive is -1:
-                ir = randrange(5)
-                print('picking random playlist:' + str(ir))
-                startTizonia(ir)
+def queryButtons():
+    global iLastButton
+    global iCounter
+    if not iLastButton == -1:
+        iCounter = iCounter + 1
+        if iCounter == 15:
+            if GPIO.input(iLastButton) == 1:
+                tmpButton = iLastButton
+                iLastButton = -1
+                buttonPressed(tmpButton)
             else:
-                print(
-                    'There is an already selected Playlist. Gonna try with Playlist ID ' + str(idActive))
-                startTizonia(idActive)
+                print('Button released oder Fehlalarm')
+                iCounter = 0
+            iLastButton = -1
+            iCounter = 0
 
-    # sCmd = 'T'  # (input())
-    if sCmd in ['n', 'N']:
-        print('Command: NEXT')
-        if not getPlayer() is None:
-            getPlayer().Next(dbus_interface='org.mpris.MediaPlayer2.Player')
-        # global sCmd
-        sCmd = 'NONO'
-    if sCmd in ['v', 'V']:
-        if not getPlayer is None:
-            vol = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'Volume',
-                                  dbus_interface='org.freedesktop.DBus.Properties')
-            print(vol)
-            sCmd = 'NONO'
-    if sCmd in ['q', 'Q']:
-        # print("Query")
-        if not getPlayer() is None:
-            stat = getPlayer().Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus',
-                                   dbus_interface='org.freedesktop.DBus.Properties')
-            print(stat)  # z.B. 'Playing
 
-            # if not stat.upper() in ['PLAYING', 'STOPPED', 'PAUSED']:
-            #    print('Tizonia not running')
-        else:
-            print('Tizonia not running')
-            sCmd = 'NONO'
-    if sCmd in ['x', 'X']:
-        killTizonia()
-        global sCMD
-        sCmd = 'NONO'
-
-    if sCmd in ['p0', 'P0']:
-        # global sCMD
-        sCmd = 'NONO'
-        idActive = 0
-        killTizonia()
-        time.sleep(.3)
-        startTizonia(0)
-    if sCmd in ['p1', 'P1']:
-        # global sCMD
-        sCmd = 'NONO'
-        idActive = 1
-        killTizonia()
-        time.sleep(.3)
-        startTizonia(1)
-    if sCmd in ['p2', 'P2']:
-        # global sCMD
-        sCmd = 'NONO'
-        idActive = 2
-        killTizonia()
-        time.sleep(.3)
-        startTizonia(2)
-    if sCmd in ['p3', 'P3']:
-        # global sCMD
-        sCmd = 'NONO'
-        idActive = 3
-        killTizonia()
-        time.sleep(.3)
-        startTizonia(3)
-    if sCmd in ['p4', 'P4']:
-        # global sCMD
-        sCmd = 'NONO'
-        idActive = 4
-        killTizonia()
-        time.sleep(.3)
-        startTizonia(4)
-
-    time.sleep(.1)
+print('Program started...')
+mixer.init()
+initGpio()
+iCounter = 0
+iErrorCounter = 0
+iLastButton = -1
+if getXml() == True:
+    iActivePlaylist = randrange(5)
+    print('Autostart. picking random playlist:' + str(iActivePlaylist))
+    killTizonia()
+    startTizonia(iActivePlaylist)
+    while True:
+        queryButtons()
+        processState()
+        time.sleep(TIMERVALUE)
+        if iErrorCounter > 5:
+            killTizonia()
+            sys.exit()
